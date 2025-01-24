334601929e1f1439a79eb0a9bbc4c435e2897ef2
SMAH1 <integral22003@yahoo.com>
2025-01-24 18:41:59 +0330

    Initialize

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SqlAmoz")]
+[assembly: AssemblyDescription("SQL training in Windows")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SqlAmoz")]
+[assembly: AssemblyCopyright("Copyright (c) 2025 SMAH1. MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4145044e-1eb5-4e58-b38f-775a193000ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ src/SelectForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace SqlAmoz
+{
+    public partial class SelectForm : Form
+    {
+        public SelectForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
